3aba6c3c845ab3313238109208af59a77b805a26
Lucas Berent <lucas.berent@gmx.at>
2018-04-25 21:35:18 +0200

    add basic project structure

---
+++ src/main/java/application/MainApplication.java
@@ -0,0 +1,12 @@
+package application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApplication {
+    private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) {
+        logger.debug("Hello world!");
+    }
+}
