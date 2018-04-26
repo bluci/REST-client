fba354fd30a95bdbb0c53431502addabd1d4137a
Lucas Berent <lucas.berent@gmx.at>
2018-04-26 10:15:28 +0200

    add ui functionality
    *add refresh button functionality
    *adapt UIController constructor injection in MainApplication

--- src/main/java/application/MainApplication.java
+++ src/main/java/application/MainApplication.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 public class MainApplication extends Application {
     private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
     private RestClient restClient = new SimpleRestClient();
-    private final String PATH_TO_PROJECT = "/Microsoft/TypeScript";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -22,15 +21,12 @@ public class MainApplication extends Application {
         primaryStage.setHeight(500);
         primaryStage.centerOnScreen();
 
-        UIController uiController = new UIController(primaryStage);
+        UIController uiController = new UIController(restClient, primaryStage);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/pullRequestViewer.fxml"));
         fxmlLoader.setControllerFactory(param -> param.isInstance(uiController) ? uiController : null);
         primaryStage.setScene(new Scene(fxmlLoader.load()));
         primaryStage.show();
         primaryStage.toFront();
-
-        //TODO remove
-        restClient.getAllOpenPullRequests(PATH_TO_PROJECT);
     }
 
     public static void main(String[] args) {
--- src/main/java/core/ui/UIController.java
+++ src/main/java/core/ui/UIController.java
@@ -1,20 +1,66 @@
 package core.ui;
 
+import core.entities.PullRequest;
+import core.rest.RestClient;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.List;
+
 public class UIController {
     private final Stage primaryStage;
     private final Logger logger = LoggerFactory.getLogger(UIController.class);
+    private final RestClient restClient;
+    private ObservableList<PullRequest> pullRequestTableData;
+    private final String PATH_TO_PROJECT = "/Microsoft/TypeScript";
 
-    public UIController(final Stage primaryStage) {
+    @FXML
+    public TableView<PullRequest> pullRequestTableView;
+    @FXML
+    public TableColumn<PullRequest, Integer> pullRequestNrCol;
+    @FXML
+    public TableColumn<PullRequest, String> pullRequestTitleCol;
 
+    public UIController(final RestClient restClient, final Stage primaryStage) {
+        this.restClient = restClient;
         this.primaryStage = primaryStage;
     }
 
     public void initialize() {
-        logger.debug("UIController");
+        pullRequestTitleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        pullRequestTableData = FXCollections.observableArrayList();
     }
 
+    public void onRefreshBtnClicked(ActionEvent actionEvent) {
+        updateTable();
+    }
+
+    private void updateTable() {
+        List<PullRequest> pullRequests;
+        try {
+            pullRequests = restClient.getAllOpenPullRequests(PATH_TO_PROJECT);
+            setPullRequestTableData(pullRequests);
+            pullRequestTableView.refresh();
+        } catch (IOException e) {
+            logger.error("error getting pull requests from restClient");
+            e.printStackTrace();
+        }
+    }
+
+    public void setPullRequestTableData(List<PullRequest> pullRequestList) {
+        pullRequestTableData.clear();
+        this.pullRequestTableData.addAll(pullRequestList);
+        pullRequestTableView.setItems(pullRequestTableData);
+    }
 }
