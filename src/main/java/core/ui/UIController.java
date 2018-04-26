c86437e9e802e289e2be755108db4a9d90f6bd5c
Lucas Berent <lucas.berent@gmx.at>
2018-04-26 12:07:18 +0200

    add alert dialog and filter functionality

--- src/main/java/core/ui/UIController.java
+++ src/main/java/core/ui/UIController.java
@@ -8,8 +8,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
@@ -17,12 +19,14 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UIController {
     private final Stage primaryStage;
     private final Logger logger = LoggerFactory.getLogger(UIController.class);
     private final RestClient restClient;
     private ObservableList<PullRequest> pullRequestTableData;
+    private List<PullRequest> allPullRequests;
     private final String PATH_TO_PROJECT = "/Microsoft/TypeScript";
 
     @FXML
@@ -31,6 +35,9 @@ public class UIController {
     public TableColumn<PullRequest, Integer> pullRequestNrCol;
     @FXML
     public TableColumn<PullRequest, String> pullRequestTitleCol;
+    @FXML
+    public TextField filterStringInput;
+
 
     public UIController(final RestClient restClient, final Stage primaryStage) {
         this.restClient = restClient;
@@ -45,19 +52,17 @@ public class UIController {
 
     public void onRefreshBtnClicked(ActionEvent actionEvent) {
         updateTable();
+        pullRequestTableView.refresh();
     }
 
     private void updateTable() {
-        List<PullRequest> pullRequests;
         try {
-            pullRequests = restClient.getAllOpenPullRequests(PATH_TO_PROJECT);
-            setPullRequestTableData(pullRequests);
-            pullRequestTableView.refresh();
-        } catch (IOException e) {
+            allPullRequests = restClient.getAllOpenPullRequests(PATH_TO_PROJECT);
+            setPullRequestTableData(allPullRequests);
+        } catch (IOException | RestException e) {
             logger.error("error getting pull requests from restClient");
             e.printStackTrace();
-        } catch (RestException e) {
-            e.printStackTrace();
+            showWarningAltert("\n" + e.getMessage());
         }
     }
 
@@ -65,5 +70,32 @@ public class UIController {
         pullRequestTableData.clear();
         pullRequestTableData.addAll(pullRequestList);
         pullRequestTableView.setItems(pullRequestTableData);
+    }
+
+    public void onApplyFilterBtnClicked(ActionEvent actionEvent) {
+        String filterString = filterStringInput.getText();
+        if (filterString.isEmpty()) {
+            logger.error("missing filter string");
+            showWarningAltert("missing filter string");
+            return;
+        }
+
+        List<PullRequest> filteredPullRequests = pullRequestTableData.stream()
+                .filter(p -> p.getTitle().matches(".*" + filterString + ".*"))
+                .collect(Collectors.toList());
+        setPullRequestTableData(filteredPullRequests);
+        pullRequestTableView.refresh();
+    }
+
+    public void onRemoveFilterBtnClicked(ActionEvent actionEvent) {
+        setPullRequestTableData(allPullRequests);
+        pullRequestTableView.refresh();
+    }
+
+    private void showWarningAltert(String msg) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setHeaderText("warning");
+        alert.setContentText(msg);
+        alert.showAndWait();
     }
 }
